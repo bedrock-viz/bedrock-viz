35d8ff48d56552195b61ec6fa941b4fe36c63bf8
Paul Grahek <paul.p.grahek@gmail.com>
2020-07-12 22:11:51 -0600

    added codecov

--- tests/example_test.cc
+++ tests/example_test.cc
@@ -1,8 +1,15 @@
+#include "nbt.h"
+
 #include <gtest/gtest.h>
 
 namespace mcpe_viz {
     TEST(Example, Test) {
         ASSERT_EQ("0", "0");
+    }
+    
+    TEST(Another, Test) {
+        std::string geojson_header = makeGeojsonHeader(0,0,false);
+        ASSERT_EQ(true,true);
     }
 }
 
